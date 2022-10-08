dea03ce6f2c1f39cb8f217aec986ddfe21a0f287
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-08 20:58:31 -0300

    Realizada as configurações iniciais do backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Liberando o "origin", a acessar a API
+app.use(cors({ credentials: true, origin: process.env.ORIGIN_IP }));
+
+app.use(express.static("public"));
+
+app.listen(5000, console.log("App funcionando!"));
