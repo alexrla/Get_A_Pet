7d8d7cd750e198aaa93322458009fd6d52dddcfc
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-08 22:20:31 -0300

    Criada as validações de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,8 +4,71 @@ module.exports = class UserController {
 
     static async register(req, res) {
 
-        res.json("Hello World!");
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const confirmPassword = req.body.confirmPassword;
+        const phone = req.body.phone;
 
+        // Validações
+        if(!name) {
+
+            res.status(422).json({ message: "O nome é obrigatório!" });
+            
+            return;
+
+        }
+
+        if(!email) {
+
+            res.status(422).json({ message: "O e-mail é obrigatório!" });
+            
+            return;
+
+        }
+
+        if(!password) {
+
+            res.status(422).json({ message: "A senha é obrigatória!" });
+            
+            return;
+
+        }
+
+        if(!confirmPassword) {
+
+            res.status(422).json({ message: "Confirme sua senha!" });
+            
+            return;
+
+        }
+
+        if(!phone) {
+
+            res.status(422).json({ message: "O telefone é obrigatório!" });
+            
+            return;
+
+        }
+
+        if(password != confirmPassword) {
+
+            res.status(422).json({ message: "A senha e a confirmação de senha, precisam ser iguais!" });
+            
+            return;
+
+        }
+
+        // Verificando a existência do usuário
+        const userExists = await User.findOne({ email: email });
+
+        if(userExists) {
+
+            res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
+            
+            return;
+
+        }
     }
 
 }
