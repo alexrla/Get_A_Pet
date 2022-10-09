a3c537574a53f8825152b01457da6318ff6dcc58
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-08 23:20:44 -0300

    Criada a funcionalidade de realizar login no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -99,4 +99,50 @@ module.exports = class UserController {
 
     }
 
+    static async login(req, res) {
+
+        const { email, password } = req.body;
+
+        if(!email) {
+
+            res.status(422).json({ message: "O e-mail é obrigatório!" });
+            
+            return;
+
+        }
+
+        if(!password) {
+
+            res.status(422).json({ message: "A senha é obrigatória!" });
+            
+            return;
+
+        }
+
+        // Verificando a existência do usuário
+        const user = await User.findOne({ email: email });
+
+        if(!user) {
+
+            res.status(422).json({ message: "E-mail não encontrado!" });
+            
+            return;
+
+        }
+
+        // Verificando a senha
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if(!checkPassword) {
+
+            res.status(422).json({ message: "Senha incorreta!" });
+            
+            return;
+
+        }
+
+        await createUserToken(user, req, res);
+
+    }
+
 }
