fc5dfb890d3417a1d5a1b1f083a535da41280d1c
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-08 22:02:42 -0300

    Criando o arquivo de controller e o arquivo de rotas dos usuários

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const UserRoutes = require("./routes/UserRoutes");
+
 const app = express();
 
 app.use(express.json());
@@ -12,5 +14,7 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: process.env.ORIGIN_HOST }));
 
 app.use(express.static("public"));
+
+app.use("/users", UserRoutes);
 
 app.listen(5000, console.log("App funcionando!"));
