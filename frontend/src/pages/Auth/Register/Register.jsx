50d0718beb2736a1b4ece7b17db4af2d51c9d86e
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-22 17:28:49 -0300

    Criada a página de cadastro

--- frontend/src/pages/Auth/Register/Register.jsx
+++ frontend/src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+
+import Input from "../../../components/form/Input/Input";
+
+// CSS
+import styles from "../../../components/form/Form.module.css";
 
 const Register = () => {
+
+    function handleChange(event) {
+
+    }
+
     return (
-        <div>Register</div>
+        <section className={styles.form_container}>
+            <h1>Realizar Cadastro</h1>
+            <form>
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite o seu telefone"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu e-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Confirmação de senha"
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirme a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Criar conta" />
+            </form>
+            <p>
+                Já tem conta? <Link to="/sign-in" className="bold">Clique aqui!</Link>
+            </p>
+        </section>
     )
+
 };
 
 export default Register;
